8609f2b2a5df3904a52cedb7fd053575526988d8
mi-c0 <59290502+mic0ishere@users.noreply.github.com>
2022-04-05 07:56:30 +0200

    fuck this

--- components/Table.js
+++ components/Table.js
@@ -1,22 +1,15 @@
 import { Table as BootstrapTable } from "react-bootstrap";
 
 function Table({ trains }) {
-  function changeTimezone(date, ianatz) {
-    const invdate = new Date(
-      date.toLocaleString("en-US", {
-        timeZone: ianatz,
+  trains = trains.map((x) => ({
+    ...x,
+    realDeparture: new Date(
+      x.departure.real.toLocaleString("en-US", {
+        timeZone: "Europe/Warsaw",
       })
-    );
-    const diff = date.getTime() - invdate.getTime();
+    ),
+  }));
 
-    return new Date(date.getTime() - diff);
-  }
-
-  const there = changeTimezone(
-    new Date(trains[0].departure.real),
-    "Europe/Warsaw"
-  );
-  console.log(there);
   return (
     <BootstrapTable striped bordered hover responsive="sm">
       <thead>
@@ -33,10 +26,10 @@ function Table({ trains }) {
             <td className="col-sm-4">{train.route.start}</td>
             <td className="col-sm-4">{train.route.end}</td>
             <td className="col-sm-3">
-              {new Date(train.departure.real).toLocaleTimeString(
-                navigator.language,
-                { hour: "2-digit", minute: "2-digit" }
-              )}
+              {train.realDeparture.toLocaleTimeString(navigator.language, {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
             </td>
             <td className="col-sm-1">{train.departure.delay / 60} min</td>
           </tr>
